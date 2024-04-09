19cccccc1257c075b5ffbf5c4b18822189e4d925
Dominic Hemken <dphemken@gmail.com>
2024-04-09 07:11:23 -0500

    add files

---
+++ OpenCraft.REST_Toolkit.Tests/TestSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenCraft.REST_Toolkit.Tests
+{
+    public static class TestSettings
+    {
+        public static bool RunUnitTests = true;
+        public static bool RunIntegrationTests = true;
+    }
+}
